a077e3dad5fc03967fbbd03a0536af52d0cbffbd
Shivani Raja <shivani_raja@hotmail.com>
2024-09-14 00:07:01 +0100

    reduce API calls

--- stock-selector/app.py
+++ stock-selector/app.py
@@ -4,6 +4,7 @@ from raw.get_profile_data import get_profile_data
 from raw.get_price_data import get_price_data
 from domain.kpi_charts import update_kpi_charts
 from domain.main_charts import update_charts
+import pandas as pd
 
 
 # define config for charts
@@ -15,18 +16,19 @@ app = Dash(__name__)
 # define layout
 app.layout = html.Div(
     [
-        html.Div(html.H1('Stock Analysis'), className='app-title'),
+        html.Div(html.H1("Stock Analysis"), className="app-title"),
         html.Div(
             dcc.Input(id="ticker", type="text", placeholder="enter ticker..."),
             className="input-ticker",
         ),
         html.Div(id="company-overview"),
+        dcc.Store(id="store-data"),  # store data here
         dcc.Slider(
             id="year-slider",
             min=2019,
             max=2023,
             value=2023,
-            marks={i: str(i) for i in range(2019, 2024)},  # Slider marks for years
+            marks={i: str(i) for i in range(2019, 2024)},
             step=1,
         ),
         html.Div(id="graphs-container"),
@@ -34,13 +36,12 @@ app.layout = html.Div(
 )
 
 
-# update company overview
+# get API data
 @app.callback(
-    Output("company-overview", "children"),
+    Output("store-data", "data"),
     Input("ticker", "value"),
 )
-def update_company_overview(ticker):
-
+def get_all_data(ticker):
     if not ticker:
         return {}
     else:
@@ -48,29 +49,46 @@ def update_company_overview(ticker):
         profile_data = get_profile_data(ticker)
         price_data = get_price_data(ticker)
         ticker_data = get_market_data(ticker)
-        children = update_kpi_charts(profile_data, price_data, ticker_data)
-        return html.Div(children)
+
+        profile_data_dict = profile_data.to_dict()
+        price_data_dict = price_data.to_dict(orient="records")
+        ticker_data_dict = ticker_data.to_dict(orient="records")
+
+    return {
+        "profile_data": profile_data_dict,
+        "price_data": price_data_dict,
+        "ticker_data": ticker_data_dict,
+    }
+
+
+# update company overview
+@app.callback(
+    Output("company-overview", "children"),
+    Input("store-data", "data"),
+)
+def update_company_overview(data):
+    profile_data = pd.Series(data["profile_data"])
+    price_data = pd.DataFrame(data["price_data"])
+    ticker_data = pd.DataFrame(data["ticker_data"])
+    children = update_kpi_charts(profile_data, price_data, ticker_data)
+    return html.Div(children)
 
 
 # update graphs
 @app.callback(
     Output("graphs-container", "children"),
-    Input("ticker", "value"),
+    Input("store-data", "data"),
     Input("year-slider", "value"),
 )
-def update_main_charts(ticker, year):
+def update_main_charts(data, year):
 
-    if not ticker:
-        return {}
-    else:
-        # get market data
-        ticker_data = get_market_data(ticker)
+    ticker_data = pd.DataFrame(data["ticker_data"])
 
-        # get currency
-        currency = ticker_data["currency_symbol"].values[0]
+    # get currency
+    currency = ticker_data["currency_symbol"].values[0]
 
-        children = update_charts(ticker_data, currency, year)
-        return html.Div(children)
+    children = update_charts(ticker_data, currency, year)
+    return html.Div(children)
 
 
 # Run the Dash app
